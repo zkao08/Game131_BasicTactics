7a2490840d22508f7decc25e8cf129414e6d28a9
Zach Kao <zkao@stumail.jccc.edu>
2019-04-08 21:49:11 -0500

    First pass at Custom Inspector for Actor finished

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -45,21 +45,38 @@ public class Actor : MonoBehaviour {
         Air
     }
 
-    public enum Position
-    {
-        left_rear_center,
-        left_rear_bottom,
-        left_rear_top,
-        left_front_center,
-        left_front_bottom,
-        left_front_top,
-        right_rear_center,
-        right_rear_bottom,
-        right_rear_top,
-        right_front_center,
-        right_front_bottom,
-        right_front_top,
-    }
+	//ORIGINAL
+	//public enum Position
+	//{
+	//    left_rear_center,
+	//    left_rear_bottom,
+	//    left_rear_top,
+	//    left_front_center,
+	//    left_front_bottom,
+	//    left_front_top,
+	//    right_rear_center,
+	//    right_rear_bottom,
+	//    right_rear_top,
+	//    right_front_center,
+	//    right_front_bottom,
+	//    right_front_top,
+	//}
+
+	public enum Position
+	{
+		left_rear_top,
+		left_rear_center,
+		left_rear_bottom,
+		left_front_top,
+		left_front_center,
+		left_front_bottom,
+		right_front_top,
+		right_front_center,
+		right_front_bottom,
+		right_rear_top,
+		right_rear_center,
+		right_rear_bottom
+	}
 
     #endregion
 
--- Assets/Scripts/Editor/ActorEditor.cs
+++ Assets/Scripts/Editor/ActorEditor.cs
@@ -7,45 +7,214 @@ using System;
 [CustomEditor(typeof(Actor))]
 public class ActorEditor : Editor
 {
-    public override void OnInspectorGUI()
-    {
-        Actor myActor = target as Actor;
+	bool isImmunitiesShowing = true;
+	bool isBoardPosShowing = true;
+	bool enemy = true;
 
-        //Actor name can not be empty, default to name of game object
-        myActor.actorName = EditorGUILayout.TextField("Actor Name", myActor.actorName);
-        if(myActor.actorName == "")
-        {
-            myActor.actorName = myActor.name;
-        }
+	public override void OnInspectorGUI()
+	{
+		Actor myActor = target as Actor;
 
-        EditorGUILayout.Space();
+		//Actor name can not be empty, default to name of game object
+		myActor.actorName = EditorGUILayout.TextField("Actor Name", myActor.actorName);
+		if (myActor.actorName == "")
+		{
+			myActor.actorName = myActor.name;
+		}
 
-        //Max HP must be > 0
-        int newHP = EditorGUILayout.DelayedIntField("Max HP", myActor.maxHitPoints);
-        if(newHP > 0)
-        {
-            myActor.maxHitPoints = newHP;
-        }
+		EditorGUILayout.Space();
 
-        //Initiative must be between 10 and 100 in steps of 5
-        const int STEP = 5;
-        myActor.initiative = STEP * (EditorGUILayout.IntSlider("Initiative", myActor.initiative, 10, 100) / STEP);
+		//Max HP must be > 0
+		int newHP = EditorGUILayout.DelayedIntField("Max HP", myActor.maxHitPoints);
+		if (newHP > 0)
+		{
+			myActor.maxHitPoints = newHP;
+		}
 
-        //Immunities
-        EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.LabelField("Immunities", GUILayout.MinWidth(100));
-        EditorGUILayout.BeginVertical();
-        EditorGUILayout.ToggleLeft("Test", false);
-        EditorGUILayout.ToggleLeft("Test2", true);
-        EditorGUILayout.EndVertical();
-        EditorGUILayout.BeginVertical();
-        EditorGUILayout.ToggleLeft("Test3", true);
-        EditorGUILayout.ToggleLeft("Test4", false);
-        EditorGUILayout.EndVertical();
-        EditorGUILayout.BeginVertical();
-        EditorGUILayout.ToggleLeft("Test5", true);
-        EditorGUILayout.ToggleLeft("Test6", false);
-        EditorGUILayout.EndVertical();
-        EditorGUILayout.EndHorizontal();
-    }
+		//Initiative must be between 10 and 100 in steps of 5
+		const int STEP = 5;
+		myActor.initiative = STEP * (EditorGUILayout.IntSlider("Initiative", myActor.initiative, 10, 100) / STEP);
+
+		//Immunities
+		Actor.ActionSource[] sourceValues = Enum.GetValues(typeof(Actor.ActionSource)) as Actor.ActionSource[];
+		string[] sourceNames = Enum.GetNames(typeof(Actor.ActionSource));
+		SelectionList<Actor.ActionSource> sources = new SelectionList<Actor.ActionSource>(sourceValues, sourceNames);
+
+		isImmunitiesShowing = EditorGUILayout.Foldout(isImmunitiesShowing, "Immunities");
+		if (isImmunitiesShowing)
+		{
+			myActor.immunities = sources.CheckboxList("Immunities", myActor.immunities, 3);
+		}
+
+		EditorGUILayout.Space();
+
+		//Action Effect
+		Actor.ActionEffect[] effectValues = Enum.GetValues(typeof(Actor.ActionEffect)) as Actor.ActionEffect[];
+		string[] effectNames = Enum.GetNames(typeof(Actor.ActionEffect));
+		SelectionList<Actor.ActionEffect> effects = new SelectionList<Actor.ActionEffect>(effectValues, effectNames);
+		
+		myActor.actionEffect = effects.RadioList("Action Effect", myActor.actionEffect, 3);
+
+		//Damage & Chance to hit
+		if(myActor.actionEffect == Actor.ActionEffect.Normal)
+		{
+			myActor.damage = EditorGUILayout.IntSlider("Damage", myActor.damage, 0, 180);
+			myActor.percentChanceToHit = EditorGUILayout.IntSlider("Percent Chance to Hit", myActor.percentChanceToHit, 0, 100);
+		}
+		else if(myActor.actionEffect == Actor.ActionEffect.Heal)
+		{
+			myActor.damage = EditorGUILayout.IntSlider("Damage", myActor.damage, 0, 180);
+			myActor.percentChanceToHit = 100;
+		}
+		else
+		{
+			myActor.damage = 0;
+			myActor.percentChanceToHit = EditorGUILayout.IntSlider("Percent Chance to Hit", myActor.percentChanceToHit, 0, 100);
+		}
+
+		//Action Effect Source
+		myActor.actionEffectSource = (Actor.ActionSource)EditorGUILayout.EnumPopup("Action Effect Source", myActor.actionEffectSource);
+
+		//Action Target
+		myActor.actionTarget = (Actor.ActionTarget)EditorGUILayout.EnumPopup("Action Target", myActor.actionTarget);
+
+		//Target Select Rule
+		myActor.targetSelectionRule = (Actor.TargetSelectionRule)EditorGUILayout.EnumPopup("Target Selection Rule", myActor.targetSelectionRule);
+
+		//Current Target
+		EditorGUILayout.LabelField("Current Target:", myActor.currentTarget.name);
+
+		EditorGUILayout.Space();
+
+		//Board Position
+		Actor.Position[] positionValues = Enum.GetValues(typeof(Actor.Position)) as Actor.Position[];
+		string[] positionNames = Enum.GetNames(typeof(Actor.Position));
+		SelectionList<Actor.Position> positions = new SelectionList<Actor.Position>(positionValues, positionNames);
+
+		isBoardPosShowing = EditorGUILayout.Foldout(isBoardPosShowing, "Board Position");
+		if (isBoardPosShowing)
+		{
+			myActor.boardPosition = positions.PositionGrid("Board Position", myActor.boardPosition, 3);
+		}
+	}
+}
+
+/** @author Steve Smith */
+class SelectionList<T> where T : IComparable
+{
+	int f = 9;
+	T[] _values;
+	string[] _labels;
+	T _selectedValue;
+
+	public T[] CheckboxList(string label, T[] initialSelections, int itemsPerCol)
+	{
+		List<T> selectedValues = new List<T>();
+		List<int> initialSelectedIndexes = new List<int>();
+		for (int i = 0; i < _values.Length; i++)
+		{
+			for (int j = 0; j < initialSelections.Length; j++)
+			{
+				if (_values[i].CompareTo(initialSelections[j]) == 0) initialSelectedIndexes.Add(i);
+			}
+		}
+
+		EditorGUILayout.BeginVertical();
+
+		EditorGUILayout.BeginHorizontal();
+		for (int r = 0; r < _values.Length; r += itemsPerCol)
+		{
+			EditorGUILayout.BeginVertical();
+			for (int i = r; i < r + itemsPerCol && i < _values.Length; i++)
+			{
+				if (GUILayout.Toggle(initialSelectedIndexes.Contains(i), _labels[i]))
+				{
+					selectedValues.Add(_values[i]);
+				}
+			}
+			EditorGUILayout.EndVertical();
+		}
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.EndVertical();
+
+		return selectedValues.ToArray();
+
+	}
+
+	public T RadioList(string label, T initialSelection, int itemsPerRow)
+	{
+		T originalSelectedValue = _selectedValue;
+		_selectedValue = initialSelection;
+		bool anyChecked = false;
+
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField(label, GUILayout.MaxWidth(100));
+
+		EditorGUILayout.BeginVertical();
+		for (int r = 0; r < _values.Length; r += itemsPerRow)
+		{
+			EditorGUILayout.BeginHorizontal();
+			for (int i = r; i < r + itemsPerRow && i < _values.Length; i++)
+			{
+				if (_values[i].CompareTo(initialSelection) == 0) originalSelectedValue = initialSelection;
+				if (GUILayout.Toggle(_values[i].CompareTo(_selectedValue) == 0, _labels[i]))
+				{
+					_selectedValue = _values[i];
+					anyChecked = true;
+				}
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+		EditorGUILayout.EndVertical();
+
+		EditorGUILayout.EndHorizontal();
+
+		if (!anyChecked) _selectedValue = originalSelectedValue;
+		return _selectedValue;
+	}
+
+	/** @author Zach Kao */
+	public T PositionGrid(string label, T initialSelection, int itemsPerCol)
+	{
+		T originalSelectedValue = _selectedValue;
+		_selectedValue = initialSelection;
+		bool anyChecked = false;
+		
+		EditorGUILayout.BeginHorizontal();
+		for (int r = 0; r < _values.Length; r += itemsPerCol)
+		{
+			if(r == (_values.Length / 2))
+			{
+				EditorGUILayout.BeginVertical(GUILayout.Width(50));
+				EditorGUILayout.Space();
+				EditorGUILayout.EndVertical();
+			}
+
+			EditorGUILayout.BeginVertical(GUILayout.Width(20));
+			for (int i = r; i < r + itemsPerCol && i < _values.Length; i++)
+			{
+				if (_values[i].CompareTo(initialSelection) == 0) originalSelectedValue = initialSelection;
+				if (GUILayout.Toggle(_values[i].CompareTo(_selectedValue) == 0, ""))
+				{
+					_selectedValue = _values[i];
+					anyChecked = true;
+				}
+			}
+			EditorGUILayout.EndVertical();
+		}
+		EditorGUILayout.EndHorizontal();
+
+		if (!anyChecked) _selectedValue = originalSelectedValue;
+		return _selectedValue;
+	}
+
+	public SelectionList(T[] values, string[] labels)
+	{
+		_values = new T[values.Length];
+		_labels = new string[labels.Length < values.Length ? values.Length : labels.Length];
+		for (int i = 0; i < _values.Length; i++) _values[i] = values[i];
+		for (int i = 0; i < _labels.Length; i++) _labels[i] = (i < labels.Length) ? labels[i] : values[i].ToString();
+		_selectedValue = _values[0];
+	}
 }
